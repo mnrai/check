79a0824b518af219dedf7d09408b287ff41ed856
Roy Rijkers <royrijkers@live.nl>
2022-10-15 21:10:08 +0200

    change

--- index.js
+++ index.js
@@ -15,21 +15,22 @@ let currentlyUsed = options.ips[0];
 const fn = async () => {
     for (let i = 0; i<ipAddresses.length; i++) {
         try {
-            const response = await fetch(`http://${ipAddresses[i]}:9933/health`);
-            const data = await response.json();
-            if (data.isSyncing !== false) {
-                throw new Error("Cannot connect to "+ ipAddresses[i])
+            if(i !== ipAddresses.length -1) {
+              const response = await fetch(`http://${ipAddresses[i]}:9933/health`);
+              const data = await response.json();
+              if (data.isSyncing !== false) {
+                  throw new Error("Cannot connect to "+ ipAddresses[i])
+              }
             }
 
             if(ipAddresses[i] !== currentlyUsed) {
                 exec(
-                  `pm2 monitor all | awk '$2 != "id" && $2 > -1 { print "pm2 info "$2" | grep \"│ name              │\""}' | bash - | awk ' { print $4}' | awk '{ print "pm2 info "$1" | grep \"script args\"  | sed \"s/│ script args       │ /pm2 delete "$1"; pm2 start \/root\/.bittensor\/bittensor\/bittensor\/_neuron\/text\/core_server\/main.py --name "$1" --time --interpreter python3 -- /g\" | sed \"s/ │//g\""}' | bash - | sed "s/${currentlyUsed}/${ipAddresses[i]}/g" | bash -`,
+                  `pm2 monitor all | awk '$2 != "id" && $2 > -1 { print "pm2 info "$2" | grep \"│ name              │\""}' | bash - | awk ' { print $4}' | awk '{ print "pm2 info "$1" | grep \"script args\"  | sed \"s/│ script args       │ /pm2 delete "$1"; pm2 start \/root\/.bittensor\/bittensor\/bittensor\/_neuron\/text\/core_server\/main.py --name "$1" --time --interpreter python3 -- /g\" | sed \"s/ │//g\""}' | bash - | sed "s/[a-zA-Z0-9.]*:9944/${ipAddresses[i]}:9944/g" | bash -`,
                   (err, stout, stderr) => {
                     if (err) {
                       console.log(err);
                     }
-                      console.log(stout);
-
+                    console.log(stout);
                   }
                 );
             }
