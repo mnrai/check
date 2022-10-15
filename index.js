f6900118983a1ab12e2eed310275df1c7a423820
Roy Rijkers <royrijkers@live.nl>
2022-10-15 21:46:26 +0200

    change

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ let currentlyUsed = options.ips[0];
 const fn = async () => {
     for (let i = 0; i<ipAddresses.length; i++) {
         try {
-            console.log({ t1: i !== ipAddresses.length - 1 });
 
             if(i !== ipAddresses.length -1) {
               const response = await fetch(`http://${ipAddresses[i]}:9933/health`);
@@ -25,9 +24,7 @@ const fn = async () => {
                   throw new Error("Cannot connect to "+ ipAddresses[i])
               }
             }
-            console.log({test1: ipAddresses[i]})
-            console.log({ test2: ipAddresses[i] !== currentlyUsed });
-            console.log({ test2: ipAddresses[i] !== currentlyUsed });
+    
             if(ipAddresses[i] !== currentlyUsed) {
               console.log(
                 `pm2 monitor all | awk '$2 != "id" && $2 > -1 { print "pm2 info "$2" | grep \\"│ name              │\\""}' | bash - | awk ' { print $4}' | awk '$1 != "index" { print "pm2 info "$1" | grep \\"script args\\" | grep \\"model_name\\" | sed \\"s/│ script args       │ /pm2 delete "$1"; pm2 start \\/root\\/.bittensor\\/bittensor\\/bittensor\\/_neuron\\/text\\/core_server\\/main.py --name "$1" --time --interpreter python3 -- /g\\" | sed \\"s/ │//g\\""}' | bash - | sed "s/[a-zA-Z0-9.]*:9944/${ipAddresses[i]}:9944/g" | bash -`
