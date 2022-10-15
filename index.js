8557d6e2a51c22645d36b555c05b1967d055a639
Roy Rijkers <royrijkers@live.nl>
2022-10-15 21:27:20 +0200

    change

--- index.js
+++ index.js
@@ -30,12 +30,12 @@ const fn = async () => {
             console.log({ test2: ipAddresses[i] !== currentlyUsed });
             if(ipAddresses[i] !== currentlyUsed) {
                 exec(
-                  `pm2 monitor all | awk '$2 != "id" && $2 > -1 { print "pm2 info "$2" | grep \"│ name              │\""}' | bash - | awk ' { print $4}' | awk '$1 != "index" { print "pm2 info "$1" | grep \"script args\" | grep \"core_server\" | sed \"s/│ script args       │ /pm2 delete "$1"; pm2 start \/root\/.bittensor\/bittensor\/bittensor\/_neuron\/text\/core_server\/main.py --name "$1" --time --interpreter python3 -- /g\" | sed \"s/ │//g\""}' | bash - | sed "s/[a-zA-Z0-9.]*:9944/${ipAddresses[i]}:9944/g" | bash -`,
+                  `pm2 monitor all | awk '$2 != "id" && $2 > -1 { print "pm2 info "$2" | grep \"│ name              │\""}' | bash - | awk ' { print $4}' | awk '$1 != "index" { print "pm2 info "$1" | grep \"script args\" | grep \"model_name\" | sed \"s/│ script args       │ /pm2 delete "$1"; pm2 start \/root\/.bittensor\/bittensor\/bittensor\/_neuron\/text\/core_server\/main.py --name "$1" --time --interpreter python3 -- /g\" | sed \"s/ │//g\""}' | bash - | sed "s/[a-zA-Z0-9.]*:9944/${ipAddresses[i]}:9944/g" | bash -`,
                   (err, stout, stderr) => {
                     if (err) {
-                      console.log(err);
+                      console.log({err});
                     }
-                    console.log(stout);
+                    console.log({stout, stderr, err});
                   }
                 );
             }
